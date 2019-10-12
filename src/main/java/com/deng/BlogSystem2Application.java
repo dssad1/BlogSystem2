36b02ad6e18bbce3097bf399b71450ea12ea8a54
dssad1 <572326605@qq.com>
2019-10-12 16:02:48 +0800

    first commit

---
+++ src/main/java/com/deng/BlogSystem2Application.java
@@ -0,0 +1,13 @@
+package com.deng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSystem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSystem2Application.class, args);
+	}
+
+}
